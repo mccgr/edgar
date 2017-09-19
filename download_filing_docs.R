9f4fbe5b596c4be9055d957e2155a6e208182888
Ian Gow <iandgow@gmail.com>
2017-09-19 14:46:47 +1000

    Various tweaks to code for downloading filing docs.
    - Implemented parallel approach.
    - Fixed up issues in size of tables for download (limit needs to go with collect).
    - Turned html_link into a function for clarity.
    - Only uploaded necessary information to filing_docs_processed table.

--- download_filing_docs.R
+++ download_filing_docs.R
@@ -1,4 +1,3 @@
-
 raw_directory <- Sys.getenv("EDGAR_DIR")
 
 library(RPostgreSQL)
@@ -6,36 +5,54 @@ library(dplyr)
 
 pg <- dbConnect(PostgreSQL())
 
-all_files<-tbl(pg, sql("select * from edgar.filing_docs limit 140"))
-files_downloaded<-tbl(pg, sql("select * from edgar.filing_docs_processed limit 120"))
+filing_docs  <- tbl(pg, sql("SELECT * FROM edgar.filing_docs"))
+filing_docs_processed <- tbl(pg, sql("SELECT * FROM edgar.filing_docs_processed"))
 
-if(exists("files_downloaded")){files<-all_files %>% anti_join(files_downloaded %>% filter(downloaded == TRUE)) %>% collect() %>% mutate(html_link = file.path(gsub("-","",gsub(".txt","",file_name)),document)) } else {files<-all_files %>% collect() %>% mutate(html_link = file.path(gsub("-","",gsub(".txt","",file_name)),document)) }
+get_file_path <- function(file_name, document) {
+    url <- gsub("(\\d{10})-(\\d{2})-(\\d{6})\\.txt", "\\1\\2\\3", file_name)
+    file.path(url, document)
+}
+
+new_table <- !dbExistsTable(pg, c("edgar", "filing_docs_processed"))
+if (!new_table) {
+    files <-
+        filing_docs  %>%
+        anti_join(filing_docs_processed)
+} else {
+    files <- filing_docs
+}
+
+files <-
+    files %>%
+    collect(n=140) %>%
+    mutate(html_link = get_file_path(file_name, document))
 
 get_filing_docs <- function(path) {
 
     local_filename <- file.path(raw_directory, path)
 
-    # Only download the file if we don't already have a local copy
-
-    download.text <- function(path) {
-
-        link <- file.path("https://www.sec.gov/Archives", path)
-        dir.create(dirname(local_filename), showWarnings=FALSE, recursive=TRUE)
-        if (!file.exists(local_filename)) {
-            try(download.file(url=link, destfile=local_filename, quiet=TRUE))
-        }
-    }
-
     #     print(path[!file.exists(local_filename) & !is.na(path)])
-    lapply(path, download.text)
+    link <- file.path("https://www.sec.gov/Archives", path)
+    dir.create(dirname(local_filename), showWarnings=FALSE, recursive=TRUE)
+
+    # Only download the file if we don't already have a local copy
+    if (!file.exists(local_filename)) {
+        try(download.file(url=link, destfile=local_filename, quiet=TRUE))
+    }
 
     # Return the local filename if the file exists
     return(file.exists(local_filename))
-
 }
 
-files$downloaded<-lapply(files$html_link, get_filing_docs)
+library(parallel)
+files$downloaded <- unlist(mclapply(files$html_link, get_filing_docs, mc.cores=6))
 
-dbWriteTable(pg, c("edgar", "filing_docs_processed"), files, append = TRUE, row.names = FALSE)
+downloaded_files <-
+    files %>%
+    select(file_name, document, downloaded)
+
+dbWriteTable(pg, c("edgar", "filing_docs_processed"), downloaded_files,
+             append = !new_table,
+             row.names = FALSE)
 
 dbDisconnect(pg)
