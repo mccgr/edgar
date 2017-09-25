d6c739199739c616591a68b5b5a9d8bee41f6724
Ian Gow <iandgow@gmail.com>
2017-09-25 12:12:42 +1000

    Several changes to download_filings code.
    - Process files in batches for robustness.
    - Set download directory to /home/shared/.
    - Created function to get list of filing documents.
    - Used regular expression to focus on HTML documents.

--- download_filing_docs.R
+++ download_filing_docs.R
@@ -1,35 +1,41 @@
-
-Sys.setenv(EDGAR_DIR="shared")
+Sys.setenv(EDGAR_DIR="/home/shared")
 raw_directory <- Sys.getenv("EDGAR_DIR")
 
 library(RPostgreSQL)
 library(dplyr)
 library(tools)
 
-pg <- dbConnect(PostgreSQL())
+get_file_list <- function(num_files = Inf) {
 
-filing_docs  <- tbl(pg, sql("SELECT * FROM edgar.filing_docs"))
-filing_docs_processed <- tbl(pg, sql("SELECT * FROM edgar.filing_docs_processed"))
+    pg <- dbConnect(PostgreSQL())
 
-get_file_path <- function(file_name, document) {
-    url <- gsub("(\\d{10})-(\\d{2})-(\\d{6})\\.txt", "\\1\\2\\3", file_name)
-    file.path(url, document)
-}
+    filing_docs  <- tbl(pg, sql("SELECT * FROM edgar.filing_docs"))
+    filing_docs_processed <- tbl(pg, sql("SELECT * FROM edgar.filing_docs_processed"))
 
-new_table <- !dbExistsTable(pg, c("edgar", "filing_docs_processed"))
-if (!new_table) {
+    get_file_path <- function(file_name, document) {
+        url <- gsub("(\\d{10})-(\\d{2})-(\\d{6})\\.txt", "\\1\\2\\3", file_name)
+        file.path(url, document)
+    }
+
+    new_table <- !dbExistsTable(pg, c("edgar", "filing_docs_processed"))
+    if (!new_table) {
+        files <-
+            filing_docs  %>%
+            anti_join(filing_docs_processed)
+    } else {
+        files <- filing_docs
+    }
+
     files <-
-        filing_docs  %>%
-        anti_join(filing_docs_processed)
-} else {
-    files <- filing_docs
-}
+        files %>%
+        filter(document %~*% "htm$") %>%
+        collect(n = num_files) %>%
+        mutate(html_link = get_file_path(file_name, document))
 
-files <-
-    files %>%
-    collect %>%
-    filter(file_ext(document)=="htm") %>%
-    mutate(html_link = get_file_path(file_name, document))
+    dbDisconnect(pg)
+
+    return(files)
+}
 
 get_filing_docs <- function(path) {
 
@@ -48,16 +54,25 @@ get_filing_docs <- function(path) {
     return(file.exists(local_filename))
 }
 
-
 library(parallel)
-system.time(files$downloaded <- unlist(mclapply(files$html_link, get_filing_docs, mc.cores=6)))
+while (nrow(files <- get_file_list(num_files = 100))>0) {
+    print("Getting files...")
+    st <- system.time(files$downloaded <-
+                    unlist(mclapply(files$html_link, get_filing_docs,
+                                    mc.cores=12)))
 
-downloaded_files <-
-    files %>%
-    select(file_name, document, downloaded)
+    print(sprintf("Downloaded %d files in %3.2f seconds",
+                  nrow(files), st[["elapsed"]]))
 
-dbWriteTable(pg, c("edgar", "filing_docs_processed"), downloaded_files,
+    downloaded_files <-
+        files %>%
+        select(file_name, document, downloaded)
+
+    pg <- dbConnect(PostgreSQL())
+    dbWriteTable(pg, c("edgar", "filing_docs_processed"), downloaded_files,
              append = !new_table,
              row.names = FALSE)
 
-dbDisconnect(pg)
+    dbDisconnect(pg)
+}
+
