90713c694265744babbc9aa9b3af73f3934d422f
Ian Gow <iandgow@gmail.com>
2017-09-29 16:11:12 +1000

    Updated get_filings.R to use read_fwf function.
    - Also switched to .gz files.
    - Switched encoding to "macintosh".

--- get_filings.R
+++ get_filings.R
@@ -2,39 +2,33 @@
 getSECIndexFile <- function(year, quarter) {
 
     library(curl)
+    library(readr)
+
     # Download the zipped index file from the SEC website
-    tf <- tempfile()
+    tf <- tempfile(fileext = ".gz")
     result <- try(curl_download(
         url=paste("https://www.sec.gov/Archives/edgar/full-index/",
-                            year,"/QTR", quarter, "/company.zip",sep=""),
+                            year,"/QTR", quarter, "/company.gz",sep=""),
         destfile=tf))
 
     # If we didn't encounter and error downloading the file, parse it
     # and return as a R data frame
     if (!inherits(result, "try-error")) {
 
-        # Small function to remove leading and trailing spaces
-        trim <- function (string) {
-            Encoding(string) <- "latin1"
-            string <- enc2native(string)
-            gsub("^\\s*(.*?)\\s*$","\\1", string, perl=TRUE)
-        }
-
-        # Read the downloaded file
-        raw.data <- readLines(con=(zz<- unz(description=tf,
-                                            filename="company.idx")))
-        close(zz)
-        raw.data <- raw.data[11:length(raw.data)] # Remove the first 10 rows.
-
         # Parse the downloaded file and return the extracted data as a data frame
-        company_name <- trim(substr(raw.data,1,62))
-        form_type <- trim(substr(raw.data,63,74))
-        cik <- trim(substr(raw.data,75,86))
-        date_filed <- as.Date(substr(raw.data,87,98))
-        file_name <- trim(substr(raw.data,99,150))
-        rm(raw.data)
-        return(data.frame(company_name, form_type, cik, date_filed, file_name))
-    } else { return(NULL)}
+        temp <-
+            read_fwf(tf, fwf_cols(company_name = c(1,62),
+                                      form_type = c(63,74),
+                                      cik = c(75,86),
+                                      date_filed = c(87,98),
+                                      file_name = c(99,150)),
+                     col_types = "ccicc", skip=10,
+                     locale = locale(encoding = "macintosh")) %>%
+            mutate(date_filed = as.Date(date_filed))
+        return(temp)
+    } else {
+        return(NULL)
+    }
 }
 
 addIndexFileToDatabase <- function(data) {
