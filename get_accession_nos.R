72f5ff17a700c89638008a204489b70ae781ab08
Ian Gow <iandgow@gmail.com>
2017-09-29 16:17:31 +1000

    Revised code to do incremental updates of accession numbers.

--- get_accession_nos.R
+++ get_accession_nos.R
@@ -1,24 +1,45 @@
-library(dplyr)
+library(dplyr, warn.conflicts = FALSE)
 library(RPostgreSQL)
 
-pg <- src_postgres()
+pg <- dbConnect(PostgreSQL())
 
-dbGetQuery(pg$con, "SET work_mem='10GB'")
-filings <- tbl(pg, sql("SELECT * FROM filings.filings"))
+dbGetQuery(pg, "SET work_mem='10GB'")
+filings <- tbl(pg, sql("SELECT * FROM edgar.filings"))
+
 acc_no_regex <- "edgar/data/\\d+/(.*)\\.txt$"
-acc_nos <-
-    filings %>%
-    mutate(accession_no = regexp_replace(file_name, acc_no_regex, "\\1")) %>%
-    mutate(cik = as.integer(cik)) %>%
-    select(cik, accession_no) %>%
-    compute(indexes = c("accession_no", "cik"))
 
-shared_ciks <-
-    acc_nos %>%
-    inner_join(acc_nos, by="accession_no", suffix = c("_x", "_y")) %>%
-    filter(cik_x != cik_y) %>%
-    compute()
+if (dbExistsTable(pg, c("edgar", "accession_numbers"))) {
+    accession_numbers <- tbl(pg, sql("SELECT * FROM edgar.accession_numbers"))
 
-shared_ciks %>%
-    group_by(cik_x, cik_y) %>%
-    summarize(num_filings = n())
+    new_filings <-
+        filings %>%
+        anti_join(accession_numbers) %>%
+        select(file_name) %>%
+        compute()
+
+    acc_nos_new <-
+        new_filings %>%
+        mutate(accessionnumber = regexp_replace(file_name, acc_no_regex, "\\1")) %>%
+        select(file_name, accessionnumber) %>%
+        compute(name="accession_numbers", indexes = c("accessionnumber", "file_name"))
+
+    dbGetQuery(pg, "INSERT INTO edgar.accession_numbers SELECT * FROM accession_numbers")
+
+    dbGetQuery(pg, "DROP TABLE IF EXISTS accession_numbers")
+} else {
+    acc_nos_all <-
+        filings %>%
+            mutate(accessionnumber = regexp_replace(file_name, acc_no_regex, "\\1")) %>%
+            select(file_name, accessionnumber) %>%
+            compute(name="accession_numbers",
+                    indexes = c("accessionnumber", "file_name"),
+                    temporary = FALSE)
+
+    dbGetQuery(pg, "ALTER TABLE accession_numbers SET SCHEMA edgar")
+
+    dbGetQuery(pg, "ALTER TABLE edgar.accession_numbers OWNER TO edgar")
+    # dbGetQuery(pg, "CREATE ROLE edgar_access")
+    dbGetQuery(pg, "GRANT SELECT ON edgar.accession_numbers TO edgar_access")
+}
+
+dbDisconnect(pg)
