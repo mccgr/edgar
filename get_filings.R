7989facb8fb96d69b6cd0939eb5008ad9ae1fea6
Ben Callen <ben_callen@hotmail.com>
2018-03-19 11:10:08 +1100

    Code to update database independently of date.
    - Relates to #8.

--- get_filings.R
+++ get_filings.R
@@ -60,26 +60,119 @@ deleteIndexDataFomDatabase <- function(pg, year, quarter) {
     }
 }
 
-# Add data for years 1993 to 2017 ----
+# find the last quarter and year from the current database ----
+
 library(RPostgreSQL)
 pg <- dbConnect(PostgreSQL())
 
-for (year in 1993:2017) {
+filings <- tbl(pg, sql("SELECT * FROM edgar.filings"))
+
+last_quarter <-
+    filings %>%
+    mutate(year = date_part('year', date_filed),
+           quarter = date_part('quarter', date_filed)) %>%
+    filter(year == max(year, na.rm = TRUE)) %>%
+    summarise(year = max(year, na.rm = TRUE),
+              quarter = max(quarter, na.rm = TRUE)) %>%
+    collect()
+
+rs <- dbDisconnect(pg)
+
+# Add data for years 1993 to the last year and quarter of the current database ----
+
+library(RPostgreSQL)
+pg <- dbConnect(PostgreSQL())
+
+for (year in 1993:(last_quarter$year-1)) {
     for (quarter in 1:4) {
         deleteIndexDataFomDatabase(pg, year, quarter)
         addIndexFileToDatabase(getSECIndexFile(year, quarter))
     }
 }
+
+for (quarter in 1:last_quarter$quarter) {
+    deleteIndexDataFomDatabase(pg, last_quarter$year, quarter)
+    addIndexFileToDatabase(getSECIndexFile(last_quarter$year, quarter))
+}
+
 rs <- dbDisconnect(pg)
 
-# Add data for 2018 ----
+
+# Find the current year and quarter ----
+
+library(lubridate)
+
+current_year <- year(today())
+current_quarter <- quarter(today())
+
+
+
+# Add data up to current year and quarter ----
+
 library(RPostgreSQL)
 pg <- dbConnect(PostgreSQL())
 
-for (year in 2018) {
-    for (quarter in 1) {
-        deleteIndexDataFomDatabase(pg, year, quarter)
-        addIndexFileToDatabase(getSECIndexFile(year, quarter))
+if(current_year == last_quarter$year) {
+
+# If current_year is equal to last_quarter$year, and the current quarter is later than that of the last update to the
+# database, add data from the quarter after the last update up to and including the current quarter
+
+
+    if(current_quarter > last_quarter$quarter) {
+
+        for(quarter in (last_quarter$quarter + 1):current_quarter) {
+
+            deleteIndexDataFomDatabase(pg, current_year, quarter)
+            addIndexFileToDatabase(getSECIndexFile(current_year, quarter))
+
+        }
+
     }
+
+} else if(current_year == last_quarter$year + 1) {
+
+# If the current year is the year after the year of the last update, add data for the remaining quarters of
+# last_quarter$year ...
+
+    for(quarter in (last_quarter$quarter + 1):4) {
+
+        deleteIndexDataFomDatabase(pg, last_quarter$year, quarter)
+        addIndexFileToDatabase(getSECIndexFile(last_quarter$year, quarter))
+
+    }
+
+# then data for the quarters of the current year, up to and including the current quarter
+
+    for (quarter in 1:current_quarter) {
+        deleteIndexDataFomDatabase(pg, current_year, quarter)
+        addIndexFileToDatabase(getSECIndexFile(current_year, quarter))
+    }
+
+
+} else if(current_year > last_quarter$year + 1) {
+
+# finally, if current_year is at least 2 years greater than last_quarter$year, do same as the previous case, but add data
+# for all 4 quarters for each year in between last_quarter$year and current_year (middle for loop)
+
+    for(quarter in (last_quarter$quarter + 1):4) {
+
+        deleteIndexDataFomDatabase(pg, last_quarter$year, quarter)
+        addIndexFileToDatabase(getSECIndexFile(last_quarter$year, quarter))
+
+    }
+
+    for(year in (last_quarter$year + 1):(current_year - 1)) {
+            for (quarter in 1:4) {
+                deleteIndexDataFomDatabase(pg, year, quarter)
+                addIndexFileToDatabase(getSECIndexFile(year, quarter))
+        }
+    }
+
+    for (quarter in 1:current_quarter) {
+        deleteIndexDataFomDatabase(pg, current_year, quarter)
+        addIndexFileToDatabase(getSECIndexFile(current_year, quarter))
+    }
+
 }
+
 rs <- dbDisconnect(pg)
