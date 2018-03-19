135c2b24dafefc53629abf3bf0fc25520196bc28
Ian Gow <iandgow@gmail.com>
2018-03-19 22:12:30 +1100

    Modified code for last updated data.
    - Relates to #9.
    - Added code to push to database.
    - Added code to compare old and new data.
    - Streamlined getLastUpdate function.
    - Moved package calls to the top.
    - Added chunk markers.

--- get_filing_update_dates.R
+++ get_filing_update_dates.R
@@ -1,33 +1,83 @@
+library(rvest)
+library(lubridate)
+library(dplyr, warn.conflicts = FALSE)
+library(tidyr)
+library(RPostgreSQL)
+
+# Function to last modified data from EDGAR ----
 getLastUpdate <- function(year, quarter) {
 
-    library(rvest)
-    library(lubridate)
+    url <- paste0("https://www.sec.gov/Archives/edgar/full-index/",
+                 year, "/QTR", quarter, "/")
+
     # Scrape the html table from the website for the given year and quarter
-    filetbl <- read_html(paste("https://www.sec.gov/Archives/edgar/full-index/",
-                               year,"/QTR", quarter, "/",sep="")) %>% html_nodes("table") %>% .[[1]] %>% html_table()
-    colnames(filetbl)[[3]] <- "Last_Modified"
-
-    # company.gz corresponds to the first row of the html table, so take the first element of filetbl$Last_Modified
-    # to return the date of the last update for the year and quarter
-    return(mdy_hms(filetbl$Last_Modified[1], tz = "UTC"))
-
-
+    read_html(url) %>%
+        html_nodes("table") %>%
+        .[[1]] %>%
+        html_table() %>%
+        filter(Name == "company.gz") %>%
+        select(`Last Modified`) %>%
+        mdy_hms(tz = "UTC")
 }
 
-makeUpdatesDataframe <- function() {
+# Create table with last_modified ----
+now <- today(tz = "UTC")
+current_year <- year(now)
+current_qtr <- quarter(now)
+year <- 1993:current_year
+quarter <- 1:4L
 
-    library(lubridate)
-    library(dplyr)
+index_last_modified <-
+    crossing(year, quarter) %>%
+    filter(year < current_year |
+               (year == current_year & quarter <= current_qtr)) %>%
+    rowwise() %>%
+    mutate(last_modified = getLastUpdate(year, quarter))
 
-    date <- today(tz = "UTC")
-    numrows <- (year(date) - 1993) * 4 + quarter(date)
-    index <- 1:numrows
-    years <- 1993 + (index - 1)%/%4L
-    quarters <- 1 + (index - 1)%%4L
-    updates <- data.frame(year = years, quarter = quarters)
-    updates <- updates %>% rowwise() %>% mutate(last_modified = getLastUpdate(year, quarter))
-    updates$year <- as.integer(updates$year)
-    updates$quarter <- as.integer(updates$quarter)
-    return(updates)
+# Push results to database ----
+pg <- dbConnect(PostgreSQL())
 
+rs <- dbExecute(pg, "SET search_path TO edgar, public")
+
+rs <- dbWriteTable(pg, "index_last_modified_new",
+             index_last_modified,
+             row.names = FALSE,
+             overwrite = TRUE)
+
+index_last_modified_new <- tbl(pg, "index_last_modified_new")
+
+# Compare new data with old to identify needed index files ----
+if (dbExistsTable(pg, "index_last_modified")) {
+    index_last_modified <- tbl(pg, "index_last_modified")
+
+    to_update <-
+        index_last_modified_new %>%
+        left_join(index_last_modified,
+                  by = c("year", "quarter"),
+                  suffix = c("_new", "_old")) %>%
+        filter(is.na(last_modified_old) |
+                   last_modified_new > last_modified_old) %>%
+        collect()
+} else {
+    to_update <-
+        index_last_modified_new %>%
+        collect()
 }
+
+####
+#
+#  Insert code to update data using to_update here.
+#  The rowwise() %>% mutate() model probably works,
+#  as the function used in mutate() could have the side-effect
+#  of updating the table.
+#
+####
+
+rs <- dbWriteTable(pg, "index_last_modified",
+             index_last_modified,
+             row.names = FALSE,
+             overwrite = TRUE)
+
+dbDisconnect(pg)
+
+
