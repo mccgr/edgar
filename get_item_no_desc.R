ef23f49f792737dd126b187d14bca1fe49aada91
Ian Gow <iandgow@gmail.com>
2018-04-22 19:37:06 +1000

    Added code to get item_no descriptions.

---
+++ get_item_no_desc.R
@@ -0,0 +1,27 @@
+library(dplyr, warn.conflicts = FALSE)
+library(RPostgreSQL)
+library(stringr)
+
+pg <- dbConnect(PostgreSQL())
+
+rs <- dbExecute(pg, "SET search_path TO edgar")
+
+item_no_desc <-
+    read_html("https://www.sec.gov/fast-answers/answersform8khtm.html") %>%
+    html_table() %>%
+    .[[1]]   %>%
+    select(1, 3) %>%
+    mutate(item_no = `X1`,
+           item_desc = `X3`) %>%
+    filter(str_detect(item_no, "^Item")) %>%
+    mutate(item_no = str_extract(item_no, "(?<=^Item\\s)(.*)$")) %>%
+    mutate(item_desc = str_replace(item_desc, "\\(.*\\)", "")) %>%
+    mutate(item_desc = str_trim(item_desc))
+
+rs <- dbWriteTable(pg, "item_no_desc", item_no_desc,
+                   overwrite = TRUE, row.names = FALSE)
+
+rs <- dbGetQuery(pg, "ALTER TABLE item_no_desc OWNER TO edgar")
+rs <- dbGetQuery(pg, "GRANT SELECT ON item_no_desc TO edgar_access")
+
+dbDisconnect(pg)
