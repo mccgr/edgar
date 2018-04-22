2ed4cded030401e8f09c28fc0079bd7aa27e9244
Ian Gow <iandgow@gmail.com>
2018-04-22 16:57:37 +1000

    Tweaks to get_item_nos.R.
    - No longer use file system.
    - Use read_lines from readr package.
    - Use tibble.
    - Simplified code for item_no.
    - Use stringr.
    - Use more cores.
    - Use get_sgml_url() function.
    - Use while loop.
    - Closes #10.

--- get_item_nos.R
+++ get_item_nos.R
@@ -1,83 +1,81 @@
-library(dplyr)
-library(curl)
+library(dplyr, warn.conflicts = FALSE)
 library(RPostgreSQL)
-library(doParallel)
-library(xml2)
+library(stringr)
+library(readr)
+library(parallel)
 
 pg <- dbConnect(PostgreSQL())
 
 filing_docs  <- tbl(pg, sql("SELECT * FROM edgar.filings"))
-filing_item_nos <- tbl(pg, sql("SELECT * FROM edgar.item_no"))
 
-## IDENTIFY FILES TO READ
+# Identify files to read ----
 
 first_read <- !dbExistsTable(pg, c("edgar", "item_no"))
 
 form_types <- c("8-K")
 
-if(!first_read){
-    files_to_read <-
-        filing_docs %>%
-        filter(form_type == filing_types) %>%
-        select(cik, file_name) %>%
-        anti_join(filing_item_nos) %>%
-        collect()
-} else {
-    files_to_read <-
-        filing_docs %>%
-        filter(form_type == filing_types) %>%
-        collect()
+if (first_read) {
+    dbGetQuery(pg, "CREATE TABLE edgar.item_no (file_name text, item_no text)")
+    dbGetQuery(pg, "CREATE INDEX ON edgar.item_no (file_name)")
 }
 
+filing_item_nos <- tbl(pg, sql("SELECT * FROM edgar.item_no"))
+
+files_to_read <-
+    filing_docs %>%
+    filter(form_type %in% form_types) %>%
+    select(cik, file_name) %>%
+    anti_join(filing_item_nos, by = "file_name")
+
 # Read in files ----
+# This function was borrowed from get_filer_ciks.R
+get_sgml_url <- function(file_name) {
+    matches <- str_match(file_name, "edgar/data/(\\d+)/(\\d{10})-(\\d{2})-(\\d{6}).txt")
+    url <- paste0("https://www.sec.gov/Archives/edgar/data/",
+                  matches[2], "/",
+                  paste0(matches[3:5], collapse=""), "/",
+                  matches[3], "-", matches[4], "-", matches[5],
+                  ".hdr.sgml")
+    return(url)
+}
 
 extract_items <- function(file_name) {
-    t<-tempfile()
-    cik <- gsub('^edgar/data/|/0.*$', '', file_name)
-    file_name_sub <- gsub("\\..*", "", basename(file_name))
-    file_folder <- gsub("-", "", file_name_sub)
-    download_url <- file.path("https://www.sec.gov/Archives/edgar/data",
-                              cik, file_folder, paste0(file_name_sub,".hdr.sgml"))
-    download.file(download_url, t, quiet = TRUE)
-    text <-
-        data.frame(content = readLines(t)) %>%
-        filter(grepl("<ITEMS>", content)) %>%
-        mutate(item_no = gsub("<ITEMS>", "", content)) %>%
-        mutate(merge = T) %>%
-        select(merge,item_no)
-    file.remove(t)
+
+    download_url <- get_sgml_url(file_name)
+
+    items <-
+        tibble(file_name = file_name,
+               item_no = str_extract(read_lines(download_url), "(?<=^<ITEMS>)(.*)$")) %>%
+        filter(!is.na(item_no))
 
     file <-
-        data.frame(file_name = file_name) %>%
-        mutate(merge = T) %>%
-        left_join(text, by = "merge") %>%
-        select(-merge)
+        tibble(file_name = file_name) %>%
+        left_join(items, by = "file_name")
+
+    file
+}
+
+batch_size <- 1000L
+
+files_remaining <- files_to_read %>% count() %>% pull()
+num_batches <- ceiling(files_remaining/batch_size)
+batch_num <- 0L
+
+while(files_to_read %>% head() %>% count() %>% pull() > 0) {
+    run_group <- collect(files_to_read, n = batch_size)
+
+    batch_num <- batch_num + 1L
+    cat("Processing batch", batch_num, "of", num_batches, "... ")
+
+    sys_time <- system.time({
+        temp_results <- bind_rows(mclapply(run_group$file_name, extract_items,
+                                       mc.cores = 32, mc.preschedule = FALSE))
+
+        dbWriteTable(pg, c("edgar", "item_no"), temp_results,
+                     append = TRUE, row.names = FALSE)
+    })
+    cat(sys_time[["elapsed"]], "seconds\n")
 }
 
 dbDisconnect(pg)
-
-for(k in 1:ceiling(nrow(files_to_read)/1000)) {
-
-    run_group <-files_to_read[((k-1)*1000+1):(k*1000),]
-
-    temp_results <- as.data.frame(do.call(rbind, mclapply(run_group$file_name, extract_items,
-                                                          mc.cores = 15, mc.preschedule = FALSE)))
-
-    pg <- dbConnect(PostgreSQL())
-
-    new_table <- !dbExistsTable(pg, c("edgar", "item_no"))
-
-    if (new_table) {
-        dbWriteTable(pg, c("edgar", "item_no"), temp_results,
-                     overwrite = TRUE, row.names = FALSE)
-        dbGetQuery(pg, "CREATE INDEX ON edgar.filing_docs (file_name)")
-    } else {
-        dbWriteTable(pg, c("edgar", "item_no"), temp_results,
-                     append = TRUE, row.names = FALSE)
-    }
-
-    dbDisconnect(pg)
-
-    print(paste("uploaded batch", k, "of", ceiling(nrow(files_to_read)/1000)))
-}
 
