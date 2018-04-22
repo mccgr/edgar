d193756a9b028f2077af770125445efa9b0b90f8
Ian Gow <iandgow@gmail.com>
2018-04-23 08:29:09 +1000

    Tweaked code for filer_ciks.
    - Cleaned up code.
    - Added permissions code.
    - Made CIK an integer to match edgar.filings.

--- get_filer_ciks.R
+++ get_filer_ciks.R
@@ -1,7 +1,13 @@
 library(xml2)
 library(stringr)
+library(dplyr, warn.conflicts = FALSE)
+library(RPostgreSQL)
+library(parallel)
+
+regex <- "edgar/data/(\\d+)/(\\d{10})-(\\d{2})-(\\d{6}).txt"
+
 get_sgml_url <- function(file_name) {
-    matches <- str_match(file_name, "edgar/data/(\\d+)/(\\d{10})-(\\d{2})-(\\d{6}).txt")
+    matches <- str_match(file_name, regex)
     url <- paste0("https://www.sec.gov/Archives/edgar/data/",
                   matches[2], "/",
                   paste0(matches[3:5], collapse=""), "/",
@@ -16,49 +22,46 @@ get_filed_by_cik <- function(file_name) {
 
     try({
         url <- get_sgml_url(file_name)
-
         temp <- as_xml_document(read_html(url))
-
         filed_by <- xml_find_first(temp, ".//filed-by")
         res <- str_extract(xml_find_first(filed_by, ".//cik"), "(?<=<cik>)(\\d+)")
+        res <- as.integer(res)
     })
 
     return(res)
 }
 
-library(dplyr, warn.conflicts = FALSE)
-library(RPostgreSQL)
-library(parallel)
-
 pg <- dbConnect(PostgreSQL())
-dbGetQuery(pg, "SET search_path='edgar'")
+dbGetQuery(pg, "SET search_path TO edgar")
 
 if (!dbExistsTable(pg, "filer_ciks")) {
-    dbGetQuery(pg, "CREATE TABLE filer_ciks (file_name text, filer_cik text)")
-
-    dbGetQuery(pg, "CREATE INDEX ON filer_ciks (file_name)")
+    dbExecute(pg, "
+        CREATE TABLE filer_ciks (file_name text, filer_cik integer);
+        CREATE INDEX ON filer_ciks (file_name);
+        ALTER TABLE filer_ciks OWNER TO edgar;
+        GRANT SELECT ON filer_ciks TO edgar_access;")
 }
 
-filings <- tbl(pg, sql("SELECT * FROM filings"))
-filer_ciks <- tbl(pg, sql("SELECT * FROM filer_ciks"))
+filings <- tbl(pg, "filings")
+filer_ciks <- tbl(pg, "filer_ciks")
 
 get_file_names <- function() {
     temp <-
         filings %>%
         filter(form_type %in% c("SC 13D", "SC 13G")) %>%
-        anti_join(filer_ciks, by="file_name") %>%
+        anti_join(filer_ciks, by = "file_name") %>%
         select(file_name) %>%
-        collect(n=200) %>%
+        collect(n = 200) %>%
         distinct()
-    if (nrow(temp)>0) pull(temp)
+    if (nrow(temp) > 0) pull(temp)
 }
 
 while(length(file_name <- get_file_names()) > 0) {
 
     filer_cik <- unlist(mclapply(file_name, get_filed_by_cik, mc.cores = 10))
 
-    dbWriteTable(pg, name="filer_ciks", tibble(file_name, filer_cik),
-             append=TRUE, row.names=FALSE)
+    dbWriteTable(pg, name = "filer_ciks", tibble(file_name, filer_cik),
+                 append = TRUE, row.names = FALSE)
 }
 
 dbDisconnect(pg)
