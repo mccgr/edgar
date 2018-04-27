82be09dfaaf3eceb40ab1315590d5a34f6cf6024
Ben Callen <ben_callen@hotmail.com>
2018-04-27 17:13:10 +1000

    - Relates to #11
    - Modified extract_cusips.R to work with Edgar database instead of the Filings database

--- extract_cusips.R
+++ extract_cusips.R
@@ -5,9 +5,9 @@
 library("RPostgreSQL")
 pg <- dbConnect(PostgreSQL())
 
-if (!dbExistsTable(pg, c("filings", "cusip_cik"))) {
+if (!dbExistsTable(pg, c("edgar", "cusip_cik"))) {
     dbGetQuery(pg, "
-        CREATE TABLE filings.cusip_cik
+        CREATE TABLE edgar.cusip_cik
             (
               file_name text,
               cusip text,
@@ -16,10 +16,10 @@ if (!dbExistsTable(pg, c("filings", "cusip_cik"))) {
               format text
             )
 
-        GRANT SELECT ON TABLE filings.cusip_cik TO crsp_basic;
+        GRANT SELECT ON TABLE edgar.cusip_cik TO crsp_basic;
 
-        CREATE INDEX ON filings.cusip_cik (cusip);
-        CREATE INDEX ON filings.cusip_cik (cik);")
+        CREATE INDEX ON edgar.cusip_cik (cusip);
+        CREATE INDEX ON edgar.cusip_cik (cik);")
 }
 
 # Note that this assumes that streetevents.calls is up to date.
@@ -27,20 +27,20 @@ file_list <- dbGetQuery(pg, "
     SET work_mem='2GB';
 
     SELECT file_name
-    FROM filings.filings 
+    FROM edgar.filings
     WHERE form_type IN ('SC 13G', 'SC 13G/A', 'SC 13D', 'SC 13D/A')
-    EXCEPT 
-    SELECT file_name 
-    FROM filings.cusip_cik
+    EXCEPT
+    SELECT file_name
+    FROM edgar.cusip_cik
     ORDER BY file_name")
 
 rs <- dbDisconnect(pg)
 
 # Create function to parse a SC 13D or SC 13F filing ----
 parseFile <- function(file_name) {
-    
+
     # Parse the indicated file using a Perl script
-    system(paste("filings/cusip_ciks/extract_cusips.pl", file_name),
+    system(paste("edgar/extract_cusips.pl", file_name),
            intern = TRUE)
 }
 
