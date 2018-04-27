7239a0cf5433048a5228daaf4efda0cc78cf1643
Ben Callen <ben_callen@hotmail.com>
2018-04-27 17:41:03 +1000

    - Relates to #11
    - Modified get_13D_filings.R to work with the Edgar database rather than the Filings database

--- get_13D_filings.R
+++ get_13D_filings.R
@@ -7,18 +7,18 @@ pg <- dbConnect(PostgreSQL())
 file.list <- dbGetQuery(pg, "
     SET work_mem='1GB';
 
-    SELECT * 
-    FROM filings.filings
+    SELECT *
+    FROM edgar.filings
     WHERE form_type IN ('SC 13G', 'SC 13G/A', 'SC 13D', 'SC 13D/A')
     AND file_name NOT IN (
-        SELECT file_name 
-        FROM filings.cusip_cik)
+        SELECT file_name
+        FROM edgar.cusip_cik)
 ")
 
 dbDisconnect(pg)
 
 # Load functions to download filings ----
-source("filings/download_filing_functions.R")
+source("edgar/download_filing_functions.R")
 
 # Now, pull text files for each filing ----
 file.list$have_file <- NA
@@ -27,7 +27,7 @@ to.get <- 1:length(file.list$have_file) #
 library(parallel)
 # Get the file
 system.time({
-    file.list$have_file[to.get] <- 
+    file.list$have_file[to.get] <-
     unlist(mclapply(file.list$file_name[to.get], get_text_file,
                     mc.preschedule=FALSE, mc.cores=6))
 })
@@ -39,8 +39,8 @@ to.get <- 1:length(file.list$sgml_file) #
 library(parallel)
 # Get the file
 system.time({
-  file.list$sgml_file[to.get] <- 
-    unlist(mclapply(file.list$file_name[to.get], get_sgml_file, 
+  file.list$sgml_file[to.get] <-
+    unlist(mclapply(file.list$file_name[to.get], get_sgml_file,
                   mc.preschedule=FALSE, mc.cores=6))
 })
 
