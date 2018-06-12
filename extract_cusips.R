9c647bf1ee547388bf18fc323273dc27fdd0b424
Ian Gow <iandgow@gmail.com>
2018-06-12 15:08:53 +1000

    Tweaks to extract_cusips.R.
    - Use dplyr.
    - Run everything is a while loop.

--- extract_cusips.R
+++ extract_cusips.R
@@ -1,8 +1,8 @@
 #!/usr/bin/env Rscript
-
-# Get a list of files that need to be processed ----
 library(dplyr, warn.conflicts = FALSE)
 library(DBI)
+
+# Do basic table set-up ----
 pg <- dbConnect(RPostgreSQL::PostgreSQL())
 
 if (!dbExistsTable(pg, c("edgar", "cusip_cik"))) {
@@ -16,23 +16,33 @@ if (!dbExistsTable(pg, c("edgar", "cusip_cik"))) {
               format text
             )
 
-        GRANT SELECT ON TABLE edgar.cusip_cik TO crsp_basic;
+        GRANT SELECT ON TABLE edgar.cusip_cik TO edgar_access;
+        ALTER TABLE edgar.cusip_cik OWNER TO edgar;
 
         CREATE INDEX ON edgar.cusip_cik (cusip);
         CREATE INDEX ON edgar.cusip_cik (cik);")
 }
 
-# Note that this assumes that streetevents.calls is up to date.
-dbGetQuery(pg, "SET work_mem='2GB'")
-filings <- tbl(pg, sql("SELECT * FROM edgar.filings"))
-cusip_cik <- tbl(pg, sql("SELECT * FROM edgar.cusip_cik"))
-file_list <-
-    filings %>%
-    filter(form_type %in% c('SC 13G', 'SC 13G/A', 'SC 13D', 'SC 13D/A')) %>%
-    anti_join(cusip_cik, by="file_name") %>%
-    collect()
-
 rs <- dbDisconnect(pg)
+
+# Get a list of files that need to be processed ----
+# Note that this assumes that streetevents.calls is up to date.
+get_filing_list <- function(num_files = Inf) {
+    pg <- dbConnect(RPostgreSQL::PostgreSQL())
+
+    dbGetQuery(pg, "SET work_mem='2GB'")
+    filings <- tbl(pg, sql("SELECT * FROM edgar.filings"))
+    cusip_cik <- tbl(pg, sql("SELECT * FROM edgar.cusip_cik"))
+
+    file_list <-
+        filings %>%
+        filter(form_type %in% c('SC 13G', 'SC 13G/A', 'SC 13D', 'SC 13D/A')) %>%
+        anti_join(cusip_cik, by="file_name") %>%
+        collect(n = num_files)
+
+    rs <- dbDisconnect(pg)
+    return(file_list)
+}
 
 # Create function to parse a SC 13D or SC 13F filing ----
 parseFile <- function(file_name) {
@@ -44,6 +54,8 @@ parseFile <- function(file_name) {
 
 # Apply parsing function to files ----
 library(parallel)
-system.time({
-    res <- unlist(mclapply(file_list$file_name, parseFile, mc.cores=12))
-})
+while(nrows(file_list <- get_filing_list(1000)) > 0) {
+    system.time({
+        res <- unlist(mclapply(file_list$file_name, parseFile, mc.cores=12))
+    })
+}
