a20e803bbafb5704e92eec00dfdf07bd86d130e1
Ben Callen <ben_callen@hotmail.com>
2018-11-20 13:08:12 +1100

    Made amended version of get_filing_docs, get_filing_docs_alt, which extracts the html links for the documents
    -Relates to #38

--- get_filing_doc_functions.R
+++ get_filing_doc_functions.R
@@ -2,6 +2,7 @@
 library(dplyr, warn.conflicts = FALSE)
 library(RPostgreSQL, quietly = TRUE)
 library(rvest, quietly = TRUE)
+library(parallel)
 
 get_index_url <- function(file_name) {
     matches <- stringr::str_match(file_name, "/(\\d+)/(\\d{10}-\\d{2}-\\d{6})")
@@ -44,6 +45,43 @@ get_filing_docs <- function(file_name) {
 
 
 }
+
+
+get_filing_docs_alt <- function(file_name) {
+
+
+    try({head_url <- get_index_url(file_name)
+
+    table_nodes <-
+        read_html(head_url, encoding="Latin1") %>%
+        html_nodes("table")
+
+    if (length(table_nodes) < 1) {
+        df <- tibble(seq = NA, description = NA, document = NA, type = NA,
+                     size = NA, file_name = file_name)
+    } else {
+
+        df <- table_nodes %>% html_table() %>% bind_rows() %>% fix_names() %>% mutate(file_name = file_name, type = as.character(type))
+
+        colnames(df) <- tolower(colnames(df))
+
+        hrefs <- table_nodes %>% html_nodes("tr") %>% html_nodes("a") %>% html_attr("href")
+
+        hrefs <- unlist(lapply(hrefs, function(x) {paste0('https://www.sec.gov', x)}))
+
+        df$html_link <- hrefs
+    }
+
+    pg <- dbConnect(PostgreSQL())
+    dbWriteTable(pg, c("edgar", "filing_docs_alt"),
+                 df, append = TRUE, row.names = FALSE)
+    dbDisconnect(pg)
+
+    return(TRUE)}, {return(FALSE)})
+
+
+}
+
 
 fix_names <- function(df) {
     colnames(df) <- tolower(colnames(df))
@@ -110,7 +148,7 @@ process_filings <- function(filings_df) {
     pg <- dbConnect(PostgreSQL())
     new_table <- !dbExistsTable(pg, c("edgar", "filing_docs"))
 
-    system.time(temp <- lapply(filings_df$file_name, get_filing_docs))
+    system.time(temp <- mclapply(filings_df$file_name, get_filing_docs, mc.cores = 24))
 
     if (new_table) {
         rs <- dbExecute(pg, "CREATE INDEX ON edgar.filing_docs (file_name)")
@@ -127,5 +165,27 @@ process_filings <- function(filings_df) {
 
 }
 
+
+process_filings_alt <- function(filings_df) {
+
+    pg <- dbConnect(PostgreSQL())
+    new_table <- !dbExistsTable(pg, c("edgar", "filing_docs_alt"))
+
+    system.time(temp <- mclapply(filings_df$file_name, get_filing_docs_alt, mc.cores = 24))
+
+    if (new_table) {
+        rs <- dbExecute(pg, "CREATE INDEX ON edgar.filing_docs_alt (file_name)")
+        rs <- dbExecute(pg, "ALTER TABLE edgar.filing_docs_alt OWNER TO edgar")
+        rs <- dbExecute(pg, "GRANT SELECT ON TABLE edgar.filing_docs_alt TO edgar_access")
+    }
+
+    rs <- dbDisconnect(pg)
+
+    temp <- unlist(temp)
+
+    return(temp)
+
+
+}
 
 
