317a6fd4f62725c55f985035176cd8b04afa328c
Ben Callen <ben_callen@hotmail.com>
2018-12-07 19:08:18 +1100

    Added edgar.forms345_reporting_owners to table list
    - Relates to #25, #26, and #41

--- process_345_xml_documents.R
+++ process_345_xml_documents.R
@@ -71,7 +71,7 @@ logical_cols <- c('got_xml', 'got_header', 'got_table1', 'got_table2', 'got_foot
                   'wrote_header', 'wrote_table1', 'wrote_table2', 'wrote_footnotes', 'wrote_footnote_indices', 'wrote_signatures')
 
 
-table_list <- c('forms345_header', 'forms345_table1', 'forms345_table2', 'forms345_footnotes',
+table_list <- c('forms345_header', 'forms345_reporting_owners', 'forms345_table1', 'forms345_table2', 'forms345_footnotes',
                 'forms345_footnote_indices', 'forms345_signatures', 'xml_process_table')
 
 pg <- dbConnect(PostgreSQL())
