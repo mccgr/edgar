d3293a05b5f111e3407ef6526461efe9df0ac0db
Ben Callen <ben_callen@hotmail.com>
2019-01-22 15:21:15 +1100

    Deleted get_index_url, fix_names and get_filing_docs from source code
    - These functions now sourced from `get_filing_doc_functions.R`
    
    - Relates to #52

--- filing_docs/scrape_filing_docs.R
+++ filing_docs/scrape_filing_docs.R
@@ -2,51 +2,8 @@
 library(dplyr, warn.conflicts = FALSE)
 library(DBI)
 library(rvest, quietly = TRUE)
+source('get_filing_doc_functions.R')
 
-get_index_url <- function(file_name) {
-    matches <- stringr::str_match(file_name, "/(\\d+)/(\\d{10}-\\d{2}-\\d{6})")
-    cik <- matches[2]
-    acc_no <- matches[3]
-    path <- stringr::str_replace_all(acc_no, "[^\\d]", "")
-
-    url <- paste0("https://www.sec.gov/Archives/edgar/data/", cik, "/", path, "/",
-                  acc_no, "-index.htm")
-    return(url)
-}
-
-fix_names <- function(df) {
-    colnames(df) <- tolower(colnames(df))
-    df
-}
-
-get_filing_docs <- function(file_name) {
-
-     head_url <- get_index_url(file_name)
-
-     table_nodes <-
-         try({
-             read_html(head_url, encoding="Latin1") %>%
-                 html_nodes("table")
-         })
-
-     if (length(table_nodes) < 1 | is(table_nodes, "try-error")) {
-         df <- tibble(seq = NA, description = NA, document = NA, type = NA,
-                      size = NA, file_name = file_name)
-         return(df)
-     } else {
-
-         df <-
-             table_nodes[1] %>%
-             html_table() %>%
-             bind_rows() %>%
-             fix_names() %>%
-             mutate(file_name = file_name,
-                    type = as.character(type))
-         df
-     }
-
-    df
-}
 
 pg <- dbConnect(RPostgres::Postgres())
 
