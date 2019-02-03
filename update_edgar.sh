b181d606221dfd5f2b62ef7e73a35deb16f9d38a
Ian Gow <iandgow@gmail.com>
2019-02-03 00:59:57 -0500

    Fixes to update_edgar.sh script.

--- update_edgar.sh
+++ update_edgar.sh
@@ -6,9 +6,9 @@ echo "Running get_accession_nos.R ..."
 echo "Running get_filer_ciks.R ..."
 ./get_filer_ciks.R
 echo "Running get_item_nos.R ..."
-./get_item_nos.R
+item_nos/get_item_nos.R
 echo "Running get_item_no_desc.R ..."
-./get_item_no_desc.R
+item_nos/get_item_no_desc.R
 # ./get_server_logs.R
 echo "Running get_filing_docs.R ..."
-./filing_docs/scrape_filing_docs.R
+filing_docs/scrape_filing_docs.R
