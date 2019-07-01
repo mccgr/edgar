3b3b0ef47cbbbe484d8f99cf65a76a9c9efb8002
Ian Gow <iandgow@gmail.com>
2019-07-01 13:08:47 -0400

    Tweak message to match file name.

--- update_edgar.sh
+++ update_edgar.sh
@@ -10,5 +10,5 @@ item_nos/get_item_nos.R
 echo "Running get_item_no_desc.R ..."
 item_nos/get_item_no_desc.R
 # ./get_server_logs.R
-echo "Running get_filing_docs.R ..."
+echo "Running scrape_filing_docs.R ..."
 filing_docs/scrape_filing_docs.R
