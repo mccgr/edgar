135776cbef8a923faef19e402b96560ad688cdae
Ian Gow <iandgow@gmail.com>
2020-06-04 13:35:00 +1000

    Fix to main script for Forms 3/4/5 call.

--- update_edgar.sh
+++ update_edgar.sh
@@ -15,4 +15,4 @@ item_nos/get_item_no_desc.R
 echo "Running scrape_filing_docs.R ..."
 filing_docs/scrape_filing_docs.R
 echo "Updating Form 3/4/5 data ..."
-forms345/update_form_345_tables.sh
+forms345/update_forms_345_tables.sh
