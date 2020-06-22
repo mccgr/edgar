87b1799c1c09fd54fd37f415fbd58e29d29431f7
Ian Gow <iandgow@gmail.com>
2020-06-23 03:19:14 +1000

    Added test code file.
    - Code is an exact copy of `scrape_filing_docs.R`.
    - Relates to #94.

---
+++ filing_docs/scrape_filing_docs_test.R
@@ -0,0 +1,63 @@
+#!/usr/bin/env Rscript
+source("filing_docs/download_filing_docs_functions.R")
+library(parallel)
+
+pg <- dbConnect(RPostgres::Postgres())
+
+rs <- dbExecute(pg, "SET search_path TO edgar, public")
+rs <- dbExecute(pg, "SET work_mem = '5GB'")
+
+filings <- tbl(pg, "filings")
+
+file_names <-
+    filings %>%
+    select(file_name)
+
+new_table <- !dbExistsTable(pg, "filing_docs")
+
+if (!new_table) {
+    filing_docs <- tbl(pg, "filing_docs")
+    def14_a <- file_names %>% anti_join(filing_docs, by = "file_name")
+} else {
+    def14_a <- file_names
+}
+
+get_file_names <- function() {
+    def14_a %>%
+        collect(n = 1000)
+}
+
+batch <- 0
+new <- lubridate::now()
+while(nrow(file_names <- get_file_names()) > 0) {
+    batch <- batch + 1
+    cat("Processing batch", batch, "\n")
+
+    temp <- mclapply(file_names$file_name, filing_docs_df, mc.cores = 6)
+    if (length(temp) > 0) {
+        df <- bind_rows(temp)
+
+        if (nrow(df) > 0) {
+            cat("Writing data ...\n")
+            dbWriteTable(pg, "filing_docs",
+                         df, append = TRUE, row.names = FALSE)
+
+        } else {
+            cat("No data ...\n")
+        }
+    }
+    old <- new; new <- lubridate::now()
+    cat(difftime(new, old, units = "secs"), "seconds\n")
+    temp <- unlist(temp)
+}
+
+if (new_table) {
+    pg <- dbConnect(RPostgres::Postgres())
+
+    rs <- dbExecute(pg, "SET search_path TO edgar, public")
+    rs <- dbExecute(pg, "CREATE INDEX ON filing_docs (file_name)")
+    rs <- dbExecute(pg, "ALTER TABLE filing_docs OWNER TO edgar")
+    rs <- dbExecute(pg, "GRANT SELECT ON TABLE filing_docs TO edgar_access")
+
+    rs <- dbDisconnect(pg)
+}
