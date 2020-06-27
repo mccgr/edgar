b77c56db1aee6348829420ed6f1872cc46bcb191
Ian Gow <iandgow@gmail.com>
2020-06-27 11:25:42 -0400

    Tweaks to filing_docs test code.
    - Mostly just clean-up before running on real data.

--- filing_docs/scrape_filing_docs_test.R
+++ filing_docs/scrape_filing_docs_test.R
@@ -3,21 +3,21 @@ library(dplyr, warn.conflicts = FALSE)
 library(DBI)
 
 target_schema <- "edgar_test"
-target_table <- "filing_docs_test"
-
+target_table <- "filing_docs"
+filings_table <- "test_sample"
 source("filing_docs/scrape_filing_docs_functions.R")
 library(parallel)
 
 pg <- dbConnect(RPostgres::Postgres())
 
-rs <- dbExecute(pg, paste0("SET search_path TO ", target_schema, ", edgar, public"))
+rs <- dbExecute(pg, paste0("SET search_path TO ", target_schema))
 rs <- dbExecute(pg, "SET work_mem = '5GB'")
 
 get_file_names <- function() {
     assign("new_table", !dbExistsTable(pg, target_table),
            env = .GlobalEnv)
 
-    filings <- tbl(pg, "test_sample")
+    filings <- tbl(pg, filings_table)
 
     file_names <-
         filings %>%
@@ -37,17 +37,18 @@ get_file_names <- function() {
 table_setup <- function() {
     pg <- dbConnect(RPostgres::Postgres())
 
-    rs <- dbExecute(pg, paste0("SET search_path TO ", target_schema, ", edgar, public"))
+    rs <- dbExecute(pg, paste0("SET search_path TO ", target_schema))
     rs <- dbExecute(pg, paste0("CREATE INDEX ON ", target_table, " (file_name)"))
     rs <- dbExecute(pg, paste0("ALTER TABLE ", target_table, " OWNER TO edgar"))
-    rs <- dbExecute(pg, paste0("GRANT SELECT ON TABLE ", target_table, " TO edgar_access"))
+    rs <- dbExecute(pg, paste0("GRANT SELECT ON TABLE ", target_table,
+                               " TO edgar_access"))
 
     rs <- dbDisconnect(pg)
 }
 
 batch <- 0
 new <- lubridate::now()
-while(nrow(file_names <- get_file_names()) > 0) {
+while (nrow(file_names <- get_file_names()) > 0) {
     batch <- batch + 1
     cat("Processing batch", batch, "\n")
 
@@ -68,7 +69,7 @@ while(nrow(file_names <- get_file_names()) > 0) {
     cat(difftime(new, old, units = "secs"), "seconds\n")
     temp <- unlist(temp)
 
-    if(new_table) {
+    if (new_table) {
         table_setup
         new_table <- FALSE
     }
